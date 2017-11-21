2875e6d98a609c749f4cf0586a42b1d25bd5844f
xlmnxp <xlmnxp@outlook.sa>
2017-11-21 20:00:53 +0300

    جلب المواضيع من المجتمع

--- app/tabs/home/home-view-model.ts
+++ app/tabs/home/home-view-model.ts
@@ -3,15 +3,15 @@ import { StackLayout } from 'ui/layouts/stack-layout';
 import { ListView } from 'ui/list-view';
 
 export class HomeViewModel extends Observable {
-    posts:any[];
-    constructor(public page:StackLayout) {
+    categories:any[];
+    constructor(public HomePage:StackLayout) {
         super();
-        this.posts = []
+        this.categories = []
         fetch("https://aosus.org/categories.json").then(res => res.json())
         .then(res =>{
-            res.category_list.categories.forEach(element => {
-                this.posts.push(element);
-                (<ListView>page.getViewById("categoriesList")).refresh();
+            res.category_list.categories.forEach(category => {
+                this.categories.push(category);
+                (<ListView>HomePage.getViewById("categoriesList")).refresh();
             });
         });
     }
